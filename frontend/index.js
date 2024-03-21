12621bbfe88efd42eabd4d6df3ab54054f7605ba
younus35 <younuscode35@gmail.com>
2024-03-22 03:16:18 +0530

    signin page

--- frontend/index.js
+++ frontend/index.js
@@ -4,14 +4,15 @@ form.addEventListener('submit',(event)=>{
     event.preventDefault();
     
     let user_details ={
-         user:event.target.username.value,
+         user:event.target.username.value,// here username is the name of the input field
          email:event.target.email.value,
          password:event.target.password.value
     }
     axios.post("http://localhost:3000/user/signup",user_details)
     .then(res => {
         if(res.data.message == 'Email already exists'){
-            alert("Email already exists");
+            const message = document.querySelector('.hidden');
+            message.innerHTML = `<p class="text-center text-danger mt-2 mb-0">email already exist</p>`;
         }
     })
     .catch(err => console.log(err));
