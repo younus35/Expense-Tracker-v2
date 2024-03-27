aefdc30715c6102b920524651ed3f191c4cadf6c
younus35 <younuscode35@gmail.com>
2024-03-28 02:51:09 +0530

    transations used to update dattabase

--- controllers/expense.js
+++ controllers/expense.js
@@ -1,5 +1,6 @@
 const Expense = require('../model/expenses');
 const User = require('../model/users');
+const sequelize = require('../util/database');
 
 exports.getExpense = async (req, res, next)=>{
     try{
@@ -13,33 +14,46 @@ exports.getExpense = async (req, res, next)=>{
 
 exports.postExpense = async (req, res, next)=>{
     try{
+        const t = await sequelize.transaction();
         const {amount,description,category} = req.body;
         // Equivalent to:
         // const amount = req.body.amount;
         // const description = req.body.description;
         // const category = req.body.category;
-        const createdExpense = await Expense.create({amount,description,category,userId:req.user.id})//propetry name and value name are same
+        const createdExpense = await Expense.create({amount,description,category,userId:req.user.id}, {transaction: t})//propetry name and value name are same
         const totalExpense = Number(req.user.totalExpenses) + Number(amount);
         await User.update({
             totalExpenses: totalExpense
         },{
-            where:{id:req.user.id}
+            where:{id:req.user.id},
+            transaction: t
         })
+        await t.commit();
         res.json(createdExpense);
      }
      catch(err){
+        await t.rollback();
          console.log(err);
      }
 }
 
 exports.deleteExpense = async (req, res, next)=>{
     try{
+      const t = await sequelize.transaction();
       const id = req.params.Id;
       const response = await Expense.findByPk(id);
-      const destroyedItem = await response.destroy();
+      await User.update({
+        totalExpenses: req.user.totalExpenses - response.amount
+      },{
+        where:{id: req.user.id},
+        transaction: t
+      })
+      const destroyedItem = await response.destroy({where:{id: id, userId:req.user.id}, transaction: t});
+      await t.commit();
       res.json(destroyedItem);
     }
     catch(err){
+        await t.rollback();
         console.log(err);
     }
 }
