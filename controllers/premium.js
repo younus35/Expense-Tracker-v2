e69fb59b5596f4bf333ee09cab28a977f964702c
younus35 <younuscode35@gmail.com>
2024-03-28 00:45:50 +0530

    optizimation of leaderboard

--- controllers/premium.js
+++ controllers/premium.js
@@ -1,29 +1,40 @@
 const User = require('../model/users');
 const Expense = require('../model/expenses');
+const sequelize = require('../util/database');
 
 
 exports.getUserLeaderBoard = async (req, res, next)=>{
     try{
-        const users = await User.findAll();
-        const expenses = await Expense.findAll();
-        
-        const userTotalExpenses = {};
-
-        expenses.forEach((expense)=>{
-            if(userTotalExpenses[expense.userId]){
-            userTotalExpenses[expense.userId] += expense.amount
-            }else{
-                userTotalExpenses[expense.userId] = expense.amount
-            }
-        })
+        const userLeaderBoardDetails = await User.findAll({ //insted of findAll() where it gives all attributes use the below
+            attributes:['id', 'name', [sequelize.fn('sum', sequelize.col('expenses.amount')), 'total']],
+            include: [{
+                model: Expense,
+                attributes:[]
+            }],
+            group:['user.id'],
+            order:[['total','DESC']]
+        });
+        // const userTotalExpenses = await Expense.findAll({
+        //     attributes: ['userId', [sequelize.fn('sum', sequelize.col('expenses.amount')), 'total']],
+        //     group:['userid'],
+            
+        // });// we have optimised in the User.findAll using the joins SO NO NEED OF IT NOW
+        // const userTotalExpenses = {};
+        // expenses.forEach((expense)=>{
+        //     if(userTotalExpenses[expense.userId]){
+        //     userTotalExpenses[expense.userId] += expense.amount
+        //     }else{
+        //         userTotalExpenses[expense.userId] = expense.amount
+        //     }
+        // })
         //console.log(userTotalExpenses);
-        var userLeaderBoardDetails = [];
-        users.forEach((user)=>{
-             userLeaderBoardDetails.push({name:user.name, total:userTotalExpenses[user.id] || 0})
-        })
-       // console.log(userLeaderBoardDetails)
-        userLeaderBoardDetails.sort((a,b)=> b.total - a.total);
-        
+        // var userLeaderBoardDetails = [];
+        // users.forEach((user)=>{
+        //      userLeaderBoardDetails.push({name:user.name, total:userTotalExpenses[user.id] || 0})
+        // })
+        // console.log(userLeaderBoardDetails)
+        // userLeaderBoardDetails.sort((a,b)=> b.total - a.total);
+
         res.status(200).json(userLeaderBoardDetails);
     }
     catch(err){
