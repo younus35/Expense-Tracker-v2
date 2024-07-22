0572eea990c7f8af3f2f881b220f4e2f2dc18d0a
younus35 <younuscode35@gmail.com>
2024-07-22 07:53:08 +0530

    reports_fix

--- controllers/report.js
+++ controllers/report.js
@@ -1,40 +1,44 @@
 const Expense = require("../model/expenses");
-const { Op } = require("sequelize");
-const AWS = require('aws-sdk');
+// const { Op } = require("sequelize");
+// const AWS = require('aws-sdk');
 const User = require("../model/users");
 
-function uploadToS3(data, filename){
-   const BUCKET_NAME = 'expensetrackerreports';
+// function uploadToS3(data, filename){
+//    const BUCKET_NAME = 'expensetrackerreports';
    
-   let s3Bucket = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY
-   })
-    var params ={
-        Bucket: BUCKET_NAME,
-        Key: filename,
-        Body: data,
-        ACL: "public-read"
-    }
-    return new Promise((resolve, reject)=>{
-      s3Bucket.upload(params, (err, res)=>{
-        if(err){
-         reject(err);
-        }else{
-         resolve(res);
-        }
-     });
-    }) 
+//    let s3Bucket = new AWS.S3({
+//     accessKeyId: process.env.AWS_ACCESS_KEY,
+//     secretAccessKey: process.env.AWS_SECRET_KEY
+//    })
+//     var params ={
+//         Bucket: BUCKET_NAME,
+//         Key: filename,
+//         Body: data,
+//         ACL: "public-read"
+//     }
+//     return new Promise((resolve, reject)=>{
+//       s3Bucket.upload(params, (err, res)=>{
+//         if(err){
+//          reject(err);
+//         }else{
+//          resolve(res);
+//         }
+//      });
+//     }) 
   
-}
+// }
 
 
 exports.dailyReports = async (req, res, next) => {
     try {
       const date = req.body.date;
 
-      const expenses = await Expense.findAll({
-        where: { date:date, userId: req.user.id },
+      // const expenses = await Expense.findAll({
+      //   where: { date:date, userId: req.user.id },
+      // });
+      const expenses = await Expense.find({
+        date: date,
+        userId: req.user._id
       });
       return res.send(expenses);
     } catch (error) {
@@ -46,14 +50,20 @@ exports.dailyReports = async (req, res, next) => {
     try {
       const month = req.body.month;
       console.log(month);
-      const expenses = await Expense.findAll({
-        where: {
-          date: {
-            [Op.like]: `%-${month}-%`,
-          },
-          userId: req.user.id,
-        },
-        raw: true,
+      // const expenses = await Expense.findAll({
+      //   where: {
+      //     date: {
+      //       [Op.like]: `%-${month}-%`,
+      //     },
+      //     userId: req.user.id,
+      //   },
+      //   raw: true,
+      // });
+      const expenses = await Expense.find({
+        userId: userId,
+        $expr: {
+          $eq: [{ $month: '$date' }, month]
+        }
       });
       return res.send(expenses);
     } catch (error) {
@@ -61,21 +71,21 @@ exports.dailyReports = async (req, res, next) => {
     }
   };
 
-exports.downloadReports = async (req, res, next) =>{
-  try{
-    const userInstance = await User.findByPk(req.user.id);
-    if(userInstance.ispremiumuser){
-       const expenses = await req.user.getExpenses();
-       const stringifiedExpenses = JSON.stringify(expenses);
-       const userId = req.user.id;
-       const filename = `Expense${userId}/${new Date()}.txt`;
-       const fileUrl = await uploadToS3(stringifiedExpenses, filename);
+// exports.downloadReports = async (req, res, next) =>{
+//   try{
+//     const userInstance = await User.findByPk(req.user.id);
+//     if(userInstance.ispremiumuser){
+//        const expenses = await req.user.getExpenses();
+//        const stringifiedExpenses = JSON.stringify(expenses);
+//        const userId = req.user.id;
+//        const filename = `Expense${userId}/${new Date()}.txt`;
+//        const fileUrl = await uploadToS3(stringifiedExpenses, filename);
 
-       await userInstance.createDownloadFile({fileUrl:fileUrl.Location});
-       res.status(200).json({fileUrl})
-  }
-}
-  catch(err){
-    console.log(err);
-  }
-}
+//        await userInstance.createDownloadFile({fileUrl:fileUrl.Location});
+//        res.status(200).json({fileUrl})
+//   }
+// }
+//   catch(err){
+//     console.log(err);
+//   }
+// }
